0dddbbf99141eaeeeb4cfbc7f8c6da7c2d04cd20
vitaliy <vkovalevskiy@omilia.com>
2020-05-17 09:35:51 +0300

    Updated SecurityConfigurer

--- src/main/java/com/example/codeengine/expense/config/SecurityConfigurer.java
+++ src/main/java/com/example/codeengine/expense/config/SecurityConfigurer.java
@@ -1,24 +1,26 @@
 package com.example.codeengine.expense.config;
 
+import com.example.codeengine.expense.filters.AuthenticationProviderImpl;
 import com.example.codeengine.expense.filters.JwtRequestFilter;
 import com.example.codeengine.expense.service.MyUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 
-    private UserDetailsService userDetailsService;
+    private MyUserDetailsService userDetailsService;
     private JwtRequestFilter jwtRequestFilter;
 
     @Autowired
@@ -29,31 +31,31 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService);
+        AuthenticationProvider provider = new AuthenticationProviderImpl(userDetailsService, getPasswordEncoder());
+        auth.authenticationProvider(provider);
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/authenticate")
-                .permitAll()
-                .anyRequest().authenticated()
+                .antMatchers("/api/authenticate").permitAll()
+                .antMatchers("/api/user/*").hasAuthority("ADMIN")
+                .anyRequest().hasAnyAuthority("USER", "ADMIN")
                 .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers(HttpMethod.OPTIONS, "/**");
+    }
+
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
-    }
-
-    @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
+        return new BCryptPasswordEncoder();
     }
 }
